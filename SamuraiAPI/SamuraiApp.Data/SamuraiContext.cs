67b3052f8003ee2112a48df30b6d26f1b4940ae7
Alex Fuller <fulleralex47@gmail.com>
2021-03-23 08:55:26 -0400

    Build out the connections between the API, domain, and data layers.

--- SamuraiAPI/SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiAPI/SamuraiApp.Data/SamuraiContext.cs
@@ -1,35 +1,19 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using SamuraiApp.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) : base(options)
+        {
+
+        }
+        
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
         public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
-
-        /// <summary>
-        /// Now required to tell the app specifically what to connect to. Abstracted connections are now gone.
-        /// </summary>
-        /// <param name="optionsBuilder">Default Db context options builder, defines db connection properties.</param>
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder
-                .UseSqlServer("Data Source= (localdb)\\MSSQLLocalDB; Initial Catalog=SamuraiAppData") //How to connect to sql server
-                .LogTo(Console.Write,
-                    new[] {DbLoggerCategory.Database.Command.Name},//Logs everything to the profiler
-                    LogLevel.Information) //Filter down to DB commands only (along with meta data about their execution)
-                .EnableSensitiveDataLogging(); //Unhides paramters passed between methods
-
-        }
 
         /// <summary>
         /// Explicitly defines the relationship between battles and samurais many to many relationship
